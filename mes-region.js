a768881391ef079062195a7803c679d297f80636
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-10-24 10:52:04 +0300

    Add files via upload

---
+++ mes-region.js
@@ -0,0 +1,340 @@
+module.exports = [
+	{
+		id: 1,
+		name: "Alaotra Mangoro",
+		district: {
+			Ambatondrazaka: {
+				commune: [
+					"Ambatondrazaka", "Feramanga Nord","Ambatondrazaka Suburbaine", "Ambandrika","Ampitatsimo","Ambohitsilaozana","Ilafy","Manakambahiny Andrefana","Antsangasanga","Ambatosoratra","Andilanatoby","Bejofo","Didy","Imerimandroso","Amparihintsokatra","Antanandava","Andromba","Manakambahiny Antsinanana","Soalazaina","Tanambao Besakay"
+				],
+				population: 324.610,
+				area: 6.077
+			},
+			Amparafaravola: {
+				commune: [
+					"Amparafaravola", "Bedidy","Ambohimandroso", "Sahamamy","Ambatomainty","Ampasikely","Andrebakely Sud","Ambohitrarivo","Anororo","Morarano Chrome","Ranomainty","Ambohijanahary","Tanambe","Beanana","Vohitsara","Amboavory","Andilana Nord","Ambodimanga","Vohimena","Andrebakely Nord"
+				],
+				population: 267.622,
+				area: 3.815
+			},
+			Andilamena:{
+				commune: [
+					"Andilamena", "Bemaitso","Antanimenabaka", "Tanananifololahy","Maroadabo","Miarinarivo","Maitsokely","Marovato"
+				],
+				population: 70.527,
+				area: 7.405
+			},
+			AnosibeAnala:{
+				commune: [
+					"Anosibe An'ala", "Ampasimaneva","Ampandroantraka", "Tratramarina","Ambatoharanana","Longozabe","Tsaravinany","Ambalaomby","Niarovana Marosampanana","Antandrokomby"
+				],
+				population: 89.205,
+				area: 2.509
+			},
+			Moramanga:{
+				commune: [
+					"Andaingo","Mangarivotra", "Antanandava","Ampasipotsy Mandialaza", "Antaniditra","Mandialaza","Fierenana","Amboasary","Lakato","Ambatovola","Beforona","Ambohidronono","Sabotsy Anjiro","Belavabary","Morarano Gara","Vodiriana","Anosibe Ifody","Andasibe","Ampasimpotsy Gara","Ambohibary","Moramanga"
+				],
+				population: 275.146,
+				area: 7.278
+			},
+		},
+		capital:"Ambatondrazaka",
+		area: 31.948 ,
+		population: 1027.110
+	},
+
+	{
+		id: 2,
+		name: "Amoron'i Mania",
+		district: {
+			Ambatofinandrahana: {
+				commune: [
+					"Fenoarivo","Soavina","Mangataboahangy","Mandrosonoro","Amborompotsy","Ambatomifanongoa","Ambondromisotra","Ambatofinandrahana","Itremo"
+				],
+				population: 155.470,
+				area: 10.386
+			},
+			Ambositra: {
+				commune: [
+					"Vohidahy", "Ambinanindrano","Ambohimitombo II","Ambohimitombo I", "Sahatsiho Ambohimanjaka","Mahazina Ambohipierenana", "Antoetra","Ambatofitorahana","Ihadilanana","Ilaka Centre","Ambalamanakana","Kianjandrakefina","Alakamisy Ambohijato","Fahizay","Marosoa","Tsarasaotra","Ivato","Imerina Imady","Andina","Ivony Miaramiasa","Ankazoambo","Ambositra II","Ambositra I"
+				],
+				population: 262.353,
+				area: 3.199
+			},
+			Fandriana:{
+				commune: [
+					"Imito","Alakamisy Ambohimahazo","Betsimisotra","Miarinavaratra","Mahazoarivo","Sandrandahy", "Ankarinoro","Tatamalaza", "Tsarazaza","Fiadanana","Sahamadio Fisakana","Milamaina","Fandriana"
+				],
+				population: 201.611,
+				area: 2.232
+			},
+			Manandriana:{
+				commune: [
+					"Andakatany","Ambatomarina","Talata Vohimena", "Vinany Andakatanikely","Anjoman'ankona","Ambohipo","Ambohimilanja","Anjoma Nandihizana","Ambohimahazo","Ambovombe Centre"
+				],
+				population: 95.594,
+				area: 2.509
+			}
+		},
+		capital:"Ambositra",
+		area: 16.141 ,
+		population: 715.027
+	},
+
+	{
+		id: 3,
+		name: "Anamalanga",
+		district: {
+			Ambohidratrimo: {
+				commune: [
+					"Antanetibe","Ambohipihaonana", "Ambato","Anjanadoria","Avaratsena","Ambohidratrimo", "Mahereza","Mahabo", "Fiadanana","Ambohimanjaka","Antsahafilo","Manjakavaradrano","Mananjara","Ampanotokana","Ampangabe","Ambatolampy","Merimandroso","Mahitsy","Ambohitrimanjaka","Ivato Airport","Ivato Firaisana","Iarinarivo","Antehiroka","Talatamaty","Anosiala"
+				],
+				population: 399.037,
+				area: 1.402
+			},
+			Andramasina: {
+				commune: [
+					"Anosibe Trimoloharano","Sabotsy Manjakavahoaka","Fitsinjovana Bakaro","Alarobia Vatosola","Tankafatra","Ambohimiadana","Antotohazo","Alatsinainy Bakaro","Mandrosoa","Andohariana","Sabotsy Ambohitromby","Andramasina"
+				],
+				population: 267.622,
+				area: 3.815
+			},
+			Anjozorobe:{
+				commune: [
+					"Beronono","Marotsipoy","Ambohimirary","Ambohibary Vohilena","Androvakely", "Amboasary Nord","Amparatanjona", "Ambatomanoina","Belanitra","Antanetibe","Analaroa","Alakamisy", "Betatao","Ambohimanarina Marovazaha", "Tsarasaotra","Ambongamarina","Mangamila","Anjozorobe",
+				],
+				population: 70.527,
+				area: 7.405
+			},
+			Ankazobe:{
+				commune: [
+					"Miantso", "Kiangara","Antakavana","Ambolotarakely","Tsaramasoandro","Fiadanana","Mahavelona","Fihaonana","Marondry","Antotohazo","Ambohitromby","Talata Angavo","Ankazobe"
+				],
+				population: 89.205,
+				area: 2.509
+			},
+			Atsimondrano:{
+				commune: [
+					"Ambatofahavalo", "Ambalavao","Tsiafahy","Androhibe","Bongatsara","Ampahitrosy","Ambohijanaka","Soavina","Alakamisy Fenoarivo", "Fenoarivo","Ankadimanga","Ampanefy","Alatsinainy Ambazaha","Antanetikely","Soalandy","Ankaraobato","Andoharanofotsy","Tanjombato","Ambavahaditokana","Itaosy","Fiombonana","Bemasoandro","Ambohidrapeto","Andranonahoatra","Anosizato Andrefana","Ampitatafika"
+				],
+				population: 275.146,
+				area: 7.278
+			},
+			Avaradrano:{
+				commune: [
+					"Ankadinandriana","Masindray","Anjeva Gara","Talata Volonondry","Viliahazo","Ambohimanga Rova","Fiaferana","Ambohimalaza Miray","Manandriana","Ambohimangakely","Sabotsy Namehana","Ambohimanambola","Aankadikely Ilafy","Alasora"
+				],
+				population: 275.146,
+				area: 7.278
+			},
+			Renivohitra:{
+				commune: [
+					"1ER Arrondissement","2E Arrondissement", "3E Arrondissement","4E Arrondissement", "5E Arrondissement","6E Arrondissement"
+				],
+				population: 275.146,
+				area: 7.278
+			},
+			Manjakandriana:{
+				commune: [
+					"Sadabe","Ampaneva","Ambohitrolomahitsy","Soavinandriana","Ankazondandy","Anjoma Betoho","Merikanjaka","Ambohitrony","Antsahalalina","Ambohitseheno","Ambohibao Sud","Ambatomena","Ambohitrandriamanitra","Ambanitsena","Anjepy","Nandihizana","Miadanandriana","Ranovao","Mantasoa","Alarobia","Ambatomanga","Ambohibary","Sambaina","Ambatolaona","Manjakandriana"
+				],
+				population: 275.146,
+				area: 7.278
+			},
+		},
+		capital:"Ambatondrazaka",
+		area: 16.911 ,
+		population: 3348.794
+	},
+	{
+		id: 4,
+		name: "Analanjirofo",
+		district: {
+			FeneriveEst: {
+				commune: [
+					"Fenerive Est", "Mahambo","Ambodimanga II", "Ampasina Maningory","Ambatoharanana","Vohilengo","Saranambana","Ampasimbe Manantsatrana","Miorimivalana","Vohipeno","Antsiatsiaka","Mahanoro"
+				],
+				population: 308.395,
+				area: 3.365
+			},
+			MananaraAvaratra: {
+				commune: [
+					"Andasibe I","Vanono","Saromaona","Antanambe","Ambatoharanana","Tanibe","Sandrakatsy","Ambodiampana","Manambolosy","Antanananivo","Antanambaobe","Imorona","Ambodivoanio","Mananara Avaratra"
+				],
+				population: 169.942 ,
+				area: 4.621
+			},
+			Maroantsetra:{
+				commune: [
+					"Anandrivola", "Ambodimanga Rantabe","Androndrono","Morafeno","Rantabe","Antsirabe Sahatany","Ankofabe","Voloina","Mahalevona","Ambanizana","Anjahana","Ambinanitelo","Ankofa","Antakotako","Andranofotsy","Manambolo","Anjanazana","Maroantsetra"
+				],
+				population: 220.973,
+				area: 6.917
+			},
+			AnosibeAnala:{
+				commune: [
+					"Sainte Marie"
+				],
+				population: 26.547,
+				area: 222 
+			},
+			SoanieranaIvongo:{
+				commune: [
+					"Antenina","Manompana","Ambahoabe","Andapafito","Ambodiampana","Antanifotsy","Fotsialanana","Soanierana Ivongo"
+				],
+				population: 136.054,
+				area: 4.814
+			},
+			Vavatenina:{
+				commune: [
+					"Vavatenina","Maromitety","Ambohibe","Ambodimangavalo","Sahatavy","Miarinarivo","Andasibe","Ambatoharanana","Anjahambe","Ampasimazava"
+				],
+				population: 173.222,
+				area: 2.826
+			},
+		},
+		capital:"Fenerive Est",
+		area: 21.930 ,
+		population: 1035.132
+	},
+	{
+		id: 5,
+		name: "Androy",
+		district: {
+			AmbovombeAndroy: {
+				commune: [
+					"Imanombo","Jafaro","Ampamata","Andalatanosy","Antanimora Atsimo","Marovato Befeno","Sihanamaro","Ambazoa", "Ambondro","Maroalopoty", "Maroalomainty","Ambonaivo","Ambohimalaza","Analamary","Ambanisarika","Anjeky Ankilikira","Erada","Tsimananada","Ambovombe"
+				],
+				population: 349.675,
+				area: 5.829
+			},
+			Bekily: {
+				commune: [
+					"Anivorano Mitsinjo","Bevitiky","Vohimanga","Beraketa","Bekitro","Tanandava","Beteza","Belindo Mahasoa","Maroviro","Ambahita","Manakompy","Tsikolaky","Tsirandrany","Ambatosola","Antsakoamaro","Anja Nord","Besakoa","Ankaranabo Nord","Morafeno Bekily"
+				],
+				population: 164.751 ,
+				area: 5.097
+			},
+			Beloha:{
+				commune: [
+					"Behabobo","Tranoroa","Marolinta","Kopoky","Tranovaho","Beloha"
+				],
+				population: 109.361 ,
+				area: 4.669
+			},
+			Tsihombe:{
+				commune: [
+					"Imongy","Antaritarika","Marovato","Anjampaly","Betanty (Faux Cap)","Nikoly","Tsihombe"
+				],
+				population: 110.147 ,
+				area: 2.223 
+			},
+		},
+		capital:"Ambovombe",
+		area: 733.933,
+		population: 19.317
+	},
+	{
+		id: 6,
+		name: "Anosy",
+		district: {
+			AmboasaryAtsimo: {
+				commune: [
+					"Ranobe","Ebelo","Tomboarivo","Marotsiraka","Tsivory","Manevy","Mahaly","Esira","Elonty","Maromby","Tranomaro","Ifotaka","Sampona","Tanandava Sud","Behara","Amboasary Atsimo"
+				],
+				population: 209.090 ,
+				area: 10.211
+			},
+			Betroka: {
+				commune: [
+					"Ambatomivary","Sakamahily","Andriandampy","Ianakafy","Nanarena Besakoa","Beampombo II","Isoanala","Iaborotra","Bekorobo","Mahasoa Est","Jangany","Beampombo I","Mahabo","Benato Toby","Ianabinda","Analamary","Ivahona","Ambalasoa","Naninora","Tsaraitso","Betroka"
+				],
+				population: 190.836 ,
+				area: 13.315
+			},
+			Taolagnaro:{
+				commune: [
+					"Soavary","Analamary","Manantenina","Ampasimena","Bevoay","Emagnobo","Ranomafana","Enakara-Haut","Fenoevo-Efita","Enaniliha","Iaboakoho","Mahatalaky","Andranobory","Analapatsy","Ankariera","Ambatoabo","Ranopiso","Ankaramena","Sarisambo","Manambaro","Mandiso","Isaka-Ivondro","Ifarantsa","Soanierana","Mandromondromotra","Ampasy Nahampoana","Fort-Dauphin"
+				],
+				population: 271.878 ,
+				area: 10.211
+			},
+		},
+		capital:"Taolagnaro (Fort Dauphin)",
+		area: 25.31 ,
+		population: 671.805
+	},
+	{
+		id: 7,
+		name: "Atsimo Andrefana",
+		district: {
+			AmpanihyOuest: {
+				commune: [
+					"Itampolo","Fotadrevo","Beroy Atsimo","Vohitany","Androka","Gogogogo","Beahitse","Belafike Haut","Ejeda","Ankilimivory","Antaly","Maniry","Ankilizato","Amborompotsy","Ankiliabo","Ampanihy Ouest"
+				],
+				population: 310.173,
+				area: 13.037
+			},
+			Ankazoabo: {
+				commune: [
+					"Ilemby","Berenty","Andranomafana","Tandrano","Fotivolo","Ankazoabo Sud"
+				],
+				population: 65.56 ,
+				area: 7.385
+			},
+			Benenitra:{
+				commune: [
+					"Ehara","Ambalavato","Ianapera","Benenitra"
+				],
+				population: 38.716 ,
+				area: 4.912
+			},
+			Beroroha:{
+				commune: [
+					"Sakena","Tanamary","Mandronarivo","Marerano","Behisatsy","Fanjakana","Bemavo","Beroroha"
+				],
+				population: 45.709 ,
+				area: 7.210 
+			},
+			BetiokyAtsimo:{
+				commune: [
+					"Lazarivo","Montifeno","Antsavoa","Belamoty","Tanambao Haut","Salobe","Marosavoa","Soaserana","Fenoandala","Soamanonga","Ankilivalo","Andranomangatsiaka","Manalobe","Bezaha","Ankazombalala","Vatolatsaka","Besely","Tongobory","Tameantsoa","Antohabato","Masiaboay","Beantake","Maroarivo Ankazomanga","Ankazomanga Ouest","Ambatry Mitsinjo","Sakamasay","Betioky Atsimo"
+				],
+				population: 205.332 ,
+				area: 9.103 
+			},
+			Morombe: {
+				commune: [
+					"Basibasy","Antanimieva","Befandriana Sud","Nosy Ambositra","Antongo Vaovao","Ambahikily","Befandefa","Cu Morombe"
+				],
+				population: 117.175 ,
+				area: 7.634
+			},
+			Sakaraha: {
+				commune: [
+					"Mitsinjo","Mikoboka","Ambinany","Andranolava","Mihavatsy","Andamasiny Vineta","Bereketa","Amboronabo","Mahaboboka","Miary Lamatihy","Miary Taheza","Sakaraha"
+				],
+				population: 113.080,
+				area: 8.447
+			},
+			ToliaryI: {
+				commune: [
+					"Besakoa","Betania","Mahavatse II","Mahavatse I","Tanambao II TSF Nord","Tanambao I"
+				],
+				population: 156.71 ,
+				area: 39
+			},
+			ToliaryII: {
+				commune: [
+					"Beheloka","Analamisampy","Ankililoaka","Milenaka","Andranovory","Manombo sud","Tsianisiha","Marofoty","Manorofify","Antanimena Onilahy","Ankilimalinike","Ambolofoty","Soalara Sud","Anakao","Saint Augustin","Andranohinaly","Ambohimahavelona","Maromiandra","Behompy","Miary Ambohibola","Betsinjaka","Belalanda","Mitsinjo Betanimena"
+				],
+				population: 264.301 ,
+				area: 6.776
+			},
+		},
+		capital:"Toliara (Tulear)",
+		area: 66.236 ,
+		population: 1316.756
+	},
+]
