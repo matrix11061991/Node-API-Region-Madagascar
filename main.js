569c063c4e077557f290beabd1279a9de63ac2db
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-10-25 17:44:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 let regions =  require('./mes-region')
-//console.log(regions)
+
 const app = express()
 const port = 3000
 
@@ -9,7 +9,7 @@ app.get('/', (req,res)=>res.send('hello from main !!'))
 app.get('/api/regions/:id', (req,res)=>{
 	const id = parseInt(req.params.id)
 	const region =  regions.find(region=>region.id === id)
-	//console.log(region.name)
+
 	res.json({region:region})
 })
 app.listen(port,()=>console.log('Serveur demare'))
