6fd482e701726c07d2b3ec507229a93867772784
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-10-30 04:53:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 let regions =  require('./mes-region')
 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req,res)=>res.send('hello from main !!'))
 
